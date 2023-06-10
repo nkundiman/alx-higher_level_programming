d41a7762ce0adf15e99cd9fa6a3d7417f10cf568
nkundiman <augunkundiman@gmail.com>
2023-06-10 09:58:08 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for val in range(0, len(row)):
+            if val ==len(row) -1:
+                print(f"{row[val]}",
+                        else:
+                        print(f"{row[val]}", end="")
+                        print()
+
